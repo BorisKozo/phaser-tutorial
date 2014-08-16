84d9abd1e860bd68e49e7ab2336e242fe26067db
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-08-16 14:28:45 +0200

    Background

--- src/game.js
+++ src/game.js
@@ -4,24 +4,36 @@
 
     var player;
     var cursors;
+    var clouds, cloudsForeground;
 
     this.preload = function () {
       game.load.image('player_ship', 'assets/sprites/player.png');
+      game.load.image('clouds', 'assets/backgrounds/clouds.png');
+      game.load.image('clouds_foreground', 'assets/backgrounds/clouds_foreground.png');
     };
     this.create = function () {
+      clouds = game.add.tileSprite(0, 0, 1600, 600, 'clouds');
+
       player = game.add.sprite(50, 10, 'player_ship');
       cursors = game.input.keyboard.createCursorKeys();
+
+      cloudsForeground = game.add.tileSprite(0, 0, 1000, 600, 'clouds_foreground');
     };
 
     this.update = function () {
-      var delta = 3 * (game.time.elapsed / 16);
+      var deltaTime = (game.time.elapsed / 16);
+      var deltaPlayerMovement = 3 * deltaTime;
+      var deltaCloudsMovement = 1.8 * deltaTime;
       
       if (cursors.up.isDown) {
-        player.y = Phaser.Math.clampBottom(player.y - delta,0);
+        player.y = Phaser.Math.clampBottom(player.y - deltaPlayerMovement,0);
       };
       if (cursors.down.isDown) {
-        player.y = Phaser.Math.clamp(player.y + delta, 0, game.camera.height - player.height);
+        player.y = Phaser.Math.clamp(player.y + deltaPlayerMovement, 0, game.camera.height - player.height);
       }
+
+      clouds.tilePosition.x -= deltaCloudsMovement;
+      cloudsForeground.tilePosition.x -= deltaCloudsMovement / 2;
     }
 
     this.render = function () {
