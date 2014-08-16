67590bc77c31445da89a77de521b80ea1fd81e74
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-08-16 11:10:13 +0200

    Added player movement

--- src/game.js
+++ src/game.js
@@ -3,16 +3,27 @@
   var Level = function () {
 
     var player;
+    var cursors;
+
     this.preload = function () {
       game.load.image('player_ship', 'assets/sprites/player.png');
     };
     this.create = function () {
       player = game.add.sprite(50, 10, 'player_ship');
+      cursors = game.input.keyboard.createCursorKeys();
     };
 
     this.update = function () {
+      var delta = 3 * (game.time.elapsed / 16);
+      
+      if (cursors.up.isDown) {
+        player.y = Phaser.Math.clampBottom(player.y - delta,0);
+      };
+      if (cursors.down.isDown) {
+        player.y = Phaser.Math.clamp(player.y + delta, 0, game.camera.height - player.height);
+      }
+    }
 
-    };
     this.render = function () {
 
     };
