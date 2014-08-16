b4abefb4938f6afe57b16ea125702b0dfd897fe6
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-08-16 10:53:59 +0200

    Create a game state

---
+++ src/game.js
@@ -0,0 +1,20 @@
+﻿(function () {
+
+  var Level = function () {
+    this.preload = function () {
+
+    };
+    this.create = function () {
+    };
+
+    this.update = function () {
+
+    };
+    this.render = function () {
+
+    };
+  }
+
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'ship-shooting-forward', new Level());
+
+})();
