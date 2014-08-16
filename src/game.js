4103bb3983de82b0763b14f8bf20c7b67f2b9135
Boris Kozorovitzky <majorzbzzn@gmail.com>
2014-08-16 20:55:08 +0200

    Enemy

--- src/game.js
+++ src/game.js
@@ -5,17 +5,21 @@
     var player;
     var cursors;
     var clouds, cloudsForeground;
+    var enemies;
 
     this.preload = function () {
       game.load.image('player_ship', 'assets/sprites/player.png');
+      game.load.image('enemy_ship', 'assets/sprites/enemy.png');
       game.load.image('clouds', 'assets/backgrounds/clouds.png');
       game.load.image('clouds_foreground', 'assets/backgrounds/clouds_foreground.png');
     };
+
     this.create = function () {
       clouds = game.add.tileSprite(0, 0, 1600, 600, 'clouds');
 
-      player = game.add.sprite(50, 10, 'player_ship');
+      player = game.add.sprite(50, 250, 'player_ship');
       cursors = game.input.keyboard.createCursorKeys();
+      enemies = game.add.group();
 
       cloudsForeground = game.add.tileSprite(0, 0, 1000, 600, 'clouds_foreground');
     };
@@ -23,17 +27,26 @@
     this.update = function () {
       var deltaTime = (game.time.elapsed / 16);
       var deltaPlayerMovement = 3 * deltaTime;
-      var deltaCloudsMovement = 1.8 * deltaTime;
-      
+      var deltaCloudsMovement = 1 * deltaTime;
+      var enemyMovement = 0.5 * deltaTime;
+
       if (cursors.up.isDown) {
-        player.y = Phaser.Math.clampBottom(player.y - deltaPlayerMovement,0);
+        player.y = Phaser.Math.clampBottom(player.y - deltaPlayerMovement, 100);
       };
       if (cursors.down.isDown) {
         player.y = Phaser.Math.clamp(player.y + deltaPlayerMovement, 0, game.camera.height - player.height);
       }
 
       clouds.tilePosition.x -= deltaCloudsMovement;
-      cloudsForeground.tilePosition.x -= deltaCloudsMovement / 2;
+      cloudsForeground.tilePosition.x -= deltaCloudsMovement * 2;
+
+      if (game.rnd.integerInRange(0, 250) === 0) {
+        enemies.create(game.camera.width + 10, game.rnd.integerInRange(100, 500), 'enemy_ship');
+      }
+
+      enemies.forEach(function (enemy) {
+        enemy.x -= enemyMovement;
+      });
     }
 
     this.render = function () {
